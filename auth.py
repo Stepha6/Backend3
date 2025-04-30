d762b8af84916298eb4730605e3a5c565f0eae7e
Stepha6 <s68106680@gmail.com>
2025-04-29 22:55:24 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, Security
+from fastapi import HTTPException, Security, Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
