39c215ac270d5e8e23dbc711ba31c391fcb21971
Stepha6 <s68106680@gmail.com>
2025-04-29 22:20:35 -0500

    Create connection.py

---
+++ connection.py
@@ -0,0 +1,25 @@
+import os
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde un archivo .env
+load_dotenv()
+
+def connect_to_mongodb(db_name, collection_name):
+    try:
+        # Obtener las credenciales de las variables de entorno
+        uri = os.getenv("MONGODB_URI")
+        
+        if not uri:
+            raise ValueError("La URI de MongoDB no está configurada en las variables de entorno")
+
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        db = client[db_name]
+        collection = db[collection_name]
+        return collection
+
+    except Exception as e:
+        # Manejo de excepciones para errores de conexión
+        print(f"Error al conectar con MongoDB: {e}")
+        return None
