80287d24dde30c58b63cc460fe7b545ba1faf2fb
Stepha6 <s68106680@gmail.com>
2025-05-21 17:43:55 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def connect_to_mongodb(db_name, collection_name):
         uri = "mongodb+srv://s68106680:Tefa2006@cluster0.4o7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-        client = client = MongoClient(uri, server_api=ServerApi('1'))
+        client = MongoClient(uri, server_api=ServerApi('1'))
         db = client[db_name]
         collection = db[collection_name]
         return collection
